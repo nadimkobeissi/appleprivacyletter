33e356747bad1a054aea14a6fe9bb0daa985efcb
Georgio Nicolas <me@georgio.xyz>
2021-08-06 23:23:37 +0300

    Close #408

--- res/js/script.js
+++ res/js/script.js
@@ -7,6 +7,11 @@ let individuals = [
 	},
 	*/
 	{
+		name: "Kaspar Kristensen",
+		url: "https://github.com/DrunkenHercules",
+		affil: "Individual"
+	},
+	{
 		name: "Sanjeev Kumar",
 		url: "https://github.com/ISanjeevKumar",
 		affil: "Individual"
